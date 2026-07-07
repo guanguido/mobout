146b64cadc277b75508c356d361a1dbb2ca6ba66
guanguido <andre.schlieper@googlemail.com>
2026-07-07 21:02:20 +0200

    Caching von expeditions.php verhindern
    Ohne explizite Cache-Control-Header konnte der Browser eine veraltete
    Antwort zwischenspeichern, sodass neu angelegte/bearbeitete Expeditionen
    bei einem normalen Reload (F5) nicht sichtbar wurden.

--- expeditions.php
+++ expeditions.php
@@ -2,4 +2,5 @@
 require __DIR__ . '/mitglieder/expeditions-lib.php';
 
 header('Content-Type: application/json; charset=utf-8');
+header('Cache-Control: no-store');
 echo json_encode(load_expeditions(), JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES);
